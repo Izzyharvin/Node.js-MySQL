863d8838b238ed0549c701b034d9004b59b17343
Israel Harvin <izzy84.ih@gmail.com>
2019-08-05 13:25:16 -0400

    have part of the .then function to update the databse

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -6,67 +6,68 @@ var connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password:'root',
-    database:'bamazon_db',
+    password: 'root',
+    database: 'bamazon_db',
 });
 
 
-connection.connect(function(error) {
-    if(error) {
+connection.connect(function (error) {
+    if (error) {
         console.log(error);
     }
     else {
         console.log("Connected!");
+        processQuestions();
     }
 
-    products();
 });
 
 
-function products() {
-    connection.query("SELECT * FROM products", function (response, error) {
-        if (error) {
-            console.log(error);
-        }
-
-        processQuestions();
-    });
-}
-
-
 function processQuestions() {
-    connection.query("SELECT * FROM products", function(response, error) {
+    connection.query("SELECT * FROM products", function (error, res) {
         if (error) {
             console.log(error);
         }
-
+        console.log(res)
         inquirer
-        .prompt([
-            {
-                name: "productId",
-                type: "input",
-                message: "What is the product ID?",
-            },
-            {
-                name: "productQuanity",
-                type: "input",
-                message: "What is the product [Quanity] you would like?",
-            },
-        ])
-        .then(function(answer) {
-            connection.query("SELECT * FROM products WHERE ?")
-            function(response, error) {
+            .prompt([
+                {
+                    name: "productId",
+                    type: "input",
+                    message: "What is the product ID?",
+                },
+                {
+                    name: "productQuanity",
+                    type: "input",
+                    message: "What is the product Quanity you would like?",
+                },
+            ])
+            .then(function (answer) {
                 var itemId = answer.productId;
                 var quantityAmount = answer.productQuanity;
-                if (quantityAmount >= itemId) {
-                    // This is where the takes the answer from the question and updates the database
-                }
-                else {
-                    console.log("Invalid Entry");
-                }
-            } 
-        });
-        
-        connection.end();
+
+                connection.query("SELECT * FROM products WHERE id = " + itemId,
+                    function (error, res) {
+                        var updatedQuantity = parseInt(stock_quantity) - parseInt(quantityAmount);
+
+                        if (res[0].stock_quantity >= quantityAmount) {
+                            connection.query("UPDATE products SET ? WHERE ?",
+                                [
+                                    {
+                                        quantityAmount: updatedQuantity
+                                   },
+                                    {
+                                        id: itemId
+                                    }
+                                ]);
+                        }
+                        else {
+                            console.log("Invalid Entry!");
+                        }
+                    })
+                    .then(function () {
+                        connection.end();
+                    });
+            });
     })
 }
